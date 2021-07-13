a821a891b4dd7c6802f8e73cc9fc8b1ffbef3852
syniox <596919077@qq.com>
2021-07-13 08:55:04 +0800

    code style unification

--- main.cpp
+++ main.cpp
@@ -6,30 +6,37 @@ static const int eps=1e-6;
 using std::cin;
 using std::vector;
 using std::map;
-//x,y: 平面直角坐标系
+// x,y: 平面直角坐标系
 
-struct vec{//向量
+struct vec{ // 向量
 	double x,y;
 	static vec get();
 };
-struct edg{//边，a为起点，b为终点，合法区域在这个边向量的左边
+struct edg{ // 边，a为起点，b为终点，合法区域在这个边向量的左边
 	vec a,b;
 };
-struct rect4{//rectangle, 长方形的四个顶点
+struct rect4{ // rectangle, 长方形的四个顶点
 	vec v[4];
 };
 
 using cls_s=vector<edg>;
 
-vector<cls_s> clss;//闭包集合
+vector<cls_s> clss; // 闭包集合
 
 vec vec::get(){
 	double x,y;
 	cin>>x>>y;
 	return (vec){x,y};
 }
+template <typename T>
+vec operator * (const vec &v,const T x){
+	return (vec){v.x*x,v.y*x};
+}
 double operator *(const vec &a,const vec &b){
 	return a.x*b.y-a.y*b.x;
+}
+vec operator + (const vec &a,const vec &b){
+	return (vec){a.x+b.x,a.y+b.y};
 }
 vec operator - (const vec &a,const vec &b){
 	return (vec){a.x-b.x,a.y-b.y};
@@ -41,17 +48,17 @@ bool operator != (const vec &a,const vec &b){
 	return a.x!=b.x||a.y!=b.y;
 }
 
-bool crossed(const edg &a,const edg &b){//线段是否相交，重合情况不考虑
+bool crossed(const edg &a,const edg &b){ // 线段是否相交，重合情况不考虑
 	return ((b.a-a.a)*(a.b-a.a))*((b.b-a.a)*(a.b-a.a))<-eps;
 }
 
-int getdr(const edg &e){//返回向量方向，右0上1左2下3
+int get_dr(const edg &e){ // 返回向量方向，右0上1左2下3
 	assert(e.a.x==e.b.x||e.a.y==e.b.y);
 	if(e.a.y==e.b.y) return (e.a.x>e.b.x)<<1;
 	else return (e.a.x>e.b.x)<<1|1;
 }
 
-bool isvalid(rect4 a,cls_s &cl){
+bool valid(rect4 a,cls_s &cl){
 	for(edg v:cl){
 		for(int i=0; i<4; ++i){
 			if(crossed((edg){a.v[i],a.v[(i+1)&3]},v))
@@ -61,29 +68,29 @@ bool isvalid(rect4 a,cls_s &cl){
 	return 1;
 }
 
-void addedg(vector<edg> &eg,map<vec,std::vector<int>> &pntidx,vec a,vec b){
+void add_edg(vector<edg> &eg,map<vec,std::vector<int>> &vec_idx,vec a,vec b){
 	eg.push_back((edg){a,b});
-	pntidx[a].push_back(eg.size());
+	vec_idx[a].push_back(eg.size());
 }
 
-void getcls(const int edgcnt){//根据题目给出的边构建rectilinear block
+void get_cls(const int edgcnt){ // 根据题目给出的边构建rectilinear block
 	vector<edg> eg;
-	map<vec,std::vector<int>> pntidx;
+	map<vec,std::vector<int>> vec_idx;
 	vector<bool> vis;
 	for(int i=1,dir; i<=edgcnt; ++i){
 		vec a=vec::get(),b=vec::get();
 		cin>>dir;
 		if(dir) std::swap(a,b);
 		if(a.x==b.x||a.y==b.y){
-			addedg(eg,pntidx,a,b);
+			add_edg(eg,vec_idx,a,b);
 		}else{
-			vec c;//斜边预处理
+			vec c; // 斜边拆分
 			if(a.x<b.x&&a.y<b.y) c=(vec){a.x,b.y};
 			if(a.x>b.x&&a.y<b.y) c=(vec){b.x,a.y};
 			if(a.x>b.x&&a.y>b.y) c=(vec){a.x,b.y};
 			if(a.x<b.x&&a.y>b.y) c=(vec){b.x,a.y};
-			addegbuk(eg,pntidx,a,c);
-			addegbuk(eg,pntidx,c,b);
+			add_edg(eg,vec_idx,a,c);
+			add_edg(eg,vec_idx,c,b);
 		}
 	}
 
@@ -92,11 +99,11 @@ void getcls(const int edgcnt){//根据题目给出的边构建rectilinear block
 		if(vis[i]) continue;
 		clss.push_back(cls_s());
 		clss.rbegin()->push_back(eg[i]);
-		for(int id=i,cnt=0; cnt<1e5; ++cnt){//防止数据不合法？
-			int curdr=getdr(eg[id]),res=id;
-			for(int j:pntidx[eg[id].b]){//WIP
+		for(int id=i,cnt=0; cnt<1e5; ++cnt){ // 防止数据不合法？
+			int curdr=get_dr(eg[id]),res=id;
+			for(int j:vec_idx[eg[id].b]){ // WIP
 				if(eg[j].a!=eg[id].b) continue;
-				if(res==id||((curdr-getdr(eg[res])+4)&3)>((curdr-getdr(eg[j])+4)&3))
+				if(res==id||((curdr-get_dr(eg[res])+4)&3)>((curdr-get_dr(eg[j])+4)&3))
 					res=j;
 			}
 			assert(res!=id);
@@ -111,24 +118,28 @@ template <typename T>
 const T cabs(const T &x){
 	return x<0?-x:x;
 }
-double getdis(const vec &a,const vec &b){
+double get_dis(const vec &a,const vec &b){
 	return cabs(a.x-b.x)+cabs(a.y-b.y);
 }
 
-vec getgreatpos(vec rct,cls_s &cl,bool &rot){//寻找某个闭包的最优位置
+vec get_great_pos(vec rct,cls_s &cl,bool &rot){//寻找某个闭包的最优位置
 	//rct描述长宽，cl表示搜寻的闭包
-	//rot记录是否旋转90度，函数返回左下角位置
+	//rot记录是否旋转90度，函数返回中心位置
 
 }
 
 int main(){
 	int edgcnt,modcnt;
 	cin>>edgcnt>>modcnt;
-	getcls(edgcnt);
+	get_cls(edgcnt);
 	while(modcnt--){
-		vec v=vec::get(),tgt=vec::get();
+		vec v=vec::get(),tgt=vec::get();//返回最优位置的中心？
+		double res=1e18;
+		vec ans;
+		bool rot;
 		for(cls_s cl:clss){
-
+			bool cur_rot;
+			vec vpos=get_great_pos(v,cl,cur_rot),center;
 		}
 	}
 	return 0;
